84868e2b773a1c26f9a53540a6780dced938c932
yamna-ali <yamna_fasih.92@hotmail.com>
2020-08-25 10:00:33 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String patientName="Stacey Manson";
+        boolean hasInsurance = false;
+        double temp = 102.2;
+        int numVisits=5;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: "+patientName);
+        System.out.println("Patient has insurance: "+hasInsurance);
+        System.out.println("Body temprature: "+ temp);
+        System.out.println("Number of visists: "+ numVisits);
+
+    }}
+/*Patient Details:
+Name: Stacey Manson
+Patient has insurance: false
+Body temperature: 102.2
+Number of visits: 5
+*
+ */
